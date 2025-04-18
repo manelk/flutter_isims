92839859387167e08eddb0c9544c62760534504c
manel kacem <manelkacem11@gmail.com>
2025-04-18 13:27:57 +0100

    initial step flutter workshop

---
+++ lib/apod_data.dart
@@ -0,0 +1,43 @@
+import 'package:my_app_isims/apod_model.dart';
+
+final List<ApodModel> apodData = [
+  ApodModel(
+    date: DateTime.parse("2025-01-01"),
+    copyright:
+        "Telescope Live, Heaven's Mirror Observatory; Processing: Chris Cantrell",
+    title: "Alpha Centauri: The Closest Star System",
+    explanation:
+        "The closest star system to the Sun is the Alpha Centauri system. Of the three stars in the system, the dimmest -- called Proxima Centauri -- is actually the nearest star...",
+    url: "https://apod.nasa.gov/apod/image/2501/AlphaCen_Cantrell_960.jpgaaa",
+    hdurl: "https://apod.nasa.gov/apod/image/2501/AlphaCen_Cantrell_3429.jpg",
+  ),
+  ApodModel(
+    date: DateTime.parse("2025-01-02"),
+    copyright: "Betul Turksoy",
+    title: "Solar Analemma 2024",
+    explanation:
+        "Recorded during 2024, this year-spanning series of images reveals a pattern in the seasonal drift of the Sun's daily motion through planet Earth's sky...",
+    url:
+        "https://apod.nasa.gov/apod/image/2501/solaranalemma2024-4-5BetulT1024.jpeg",
+    hdurl:
+        "https://apod.nasa.gov/apod/image/2501/solaranalemma2024-4-5BetulT.jpeg",
+  ),
+  ApodModel(
+    date: DateTime.parse("2025-01-03"),
+    copyright: "Josh Dury",
+    title: "Eclipse Pair",
+    explanation:
+        "Eclipses tend to come in pairs. Twice a year, during an eclipse season that lasts about 34 days, Sun, Moon, and Earth can nearly align...",
+    url: "https://apod.nasa.gov/apod/image/2501/APODEclipsePair1024.jpg",
+    hdurl: "https://apod.nasa.gov/apod/image/2501/APODEclipsePair2048.jpg",
+  ),
+  ApodModel(
+    date: DateTime.parse("2025-01-04"),
+    copyright: "Barden Ridge Observatory",
+    title: "Welcome to Perihelion",
+    explanation:
+        "Earth's orbit around the Sun is not a circle, it's an ellipse. The point along its elliptical orbit where our fair planet is closest to the Sun is called perihelion...",
+    url: "https://apod.nasa.gov/apod/image/2501/20250104PeriSolHaNeg1024.jpg",
+    hdurl: "https://apod.nasa.gov/apod/image/2501/20250104PeriSolHaNeg.jpg",
+  ),
+];
---
+++ lib/apod_model.dart
@@ -0,0 +1,17 @@
+class ApodModel {
+  String copyright;
+  DateTime date;
+  String explanation;
+  String hdurl;
+  String title;
+  String url;
+
+  ApodModel({
+    required this.copyright,
+    required this.date,
+    required this.explanation,
+    required this.hdurl,
+    required this.title,
+    required this.url,
+  });
+}
